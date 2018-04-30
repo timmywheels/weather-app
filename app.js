421c758a4159a92abbce6cb7310db92f3418f525
Tim Wheeler <tim@timwheeler.com>
2018-04-30 19:15:30 -0400

    Version 1

---
+++ app.js
@@ -0,0 +1,19 @@
+//Todo List App
+
+//Version 1 Requirements
+
+//store todos
+var todos = ['item 1', 'item 2', 'item 3'];
+
+//display todos
+console.log(todos);
+
+//add a new todo
+todos.push('item 4', 'item 5');
+
+//change a todo
+todos[0] = 'item 1 changed';
+
+//delete a todo
+todos.splice(1, 1);
+
