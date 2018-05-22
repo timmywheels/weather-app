cb33c7695f795f0c3d93b9dd8f21a29e274714df
Tim Wheeler <tim@timwheeler.com>
2018-05-22 23:42:14 +0000

    Weather App Version 1.2.3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Weather App
 
 A weather app using the [freeCodeCamp Weather API](https://fcc-weather-api.glitch.me );
+
+Check out more of my projects @ [timwheeler.com](https://timwheeler.com/projects)
--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/* global navigator */
-/* global document */
-/* global $ */
-
 var api = 'https://fcc-weather-api.glitch.me/api/current?';
 var lat;
 var lon;
@@ -10,19 +6,23 @@ var title = document.getElementById('title');
 
 document.addEventListener('DOMContentLoaded', function() {
 
-
   function getLocation() {
-    if (navigator.geolocation) {
+
+    if (navigator.geolocation) { //check if brower supports geolocation
       navigator.geolocation.getCurrentPosition(function(position) {
-        lat = "lat=" + position.coords.latitude;
-        lon = "lon=" + position.coords.longitude;
-        api += lat + '&' + lon;
-        getWeather(lat, lon);
+
+        lat = "lat=" + position.coords.latitude; //get latitude
+        lon = "lon=" + position.coords.longitude; //get longitude
+
+        api += lat + '&' + lon; //create api call url
+
+        getWeather(lat, lon); //call api
+
       });
 
     }
     else {
-      title.innerHTML = "Geolocation is not supported by this browser.";
+      title.innerHTML = "Geolocation is not supported by this browser."; //otherwise display an error
     }
   }
 
@@ -30,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var apiUrl = api;
 
     $.ajax({
+
       url: apiUrl,
       success: function(forecast) {
         $('#loader').fadeOut(500); //hide loader on success
@@ -37,26 +38,28 @@ document.addEventListener('DOMContentLoaded', function() {
         $('#icon').fadeIn(1000);
         $('.switch').fadeIn(1000);
 
-        console.log(apiUrl);
-        JSON.stringify(forecast);
+        // console.log(apiUrl);
+
         $('#title').text('Weather Report'); //set title on load
         $('#city').text(forecast.name); //set weather city
 
-        var tempInCelsius = Math.round(forecast.main.temp); //round temps to nearest whole num
-        var tempInFahrenheit = Math.round(tempInCelsius * (9 / 5) + 32);
+        var tempInCelsius = Math.round(forecast.main.temp); //temp in celsis, rounded to nearest whole num
+        var tempInFahrenheit = Math.round(tempInCelsius * (9 / 5) + 32); //convert celsius to fahrenheit
 
         $('#temp').text(tempInFahrenheit + '°F'); //set default temp as fahrenheit
 
 
         $('#tempSwitch').on('click', function(e) {
 
-          if ($('input[type=checkbox]').prop('checked') != true) { //if switch is checked, convert temp to celsius
+          if ($('input[type=checkbox]').prop('checked') != true) { //if switch is checked, temp is celsius
 
             $('#temp').text(tempInCelsius + '°C');
 
           }
           else {
-            $('#temp').text(tempInFahrenheit + '°F'); //otherwise convert temp to fahrenheit
+
+            $('#temp').text(tempInFahrenheit + '°F'); //otherwise temp is fahrenheit
+
           }
 
         });
@@ -67,74 +70,68 @@ document.addEventListener('DOMContentLoaded', function() {
           $('#icon').attr('src', forecast.weather[0].icon);
         }
 
-        else if (typeof forecast.weather[1] === 'undefined'){
+        else if (typeof forecast.weather[1] === 'undefined') {
           $('#icon').attr('src', './icons/00.png');
         }
-
-        // else if (forecast.weather[1].hasOwnProperty('icon')) {
-          if (forecast.weather[1].icon === '01d') {
-            $('#icon').attr('src', './icons/01d.png');
-          }
-          if (forecast.weather[1].icon === '01n') {
-            $('#icon').attr('src', './icons/01n.png');
-          }
-          if (forecast.weather[1].icon === '02d') {
-            $('#icon').attr('src', './icons/02d.png');
-          }
-          if (forecast.weather[1].icon === '02n') {
-            $('#icon').attr('src', './icons/02n.png');
-          }
-          if (forecast.weather[1].icon === '03d') {
-            $('#icon').attr('src', './icons/03d.png');
-          }
-          if (forecast.weather[1].icon === '03n') {
-            $('#icon').attr('src', './icons/03n.png');
-          }
-          if (forecast.weather[1].icon === '04d') {
-            $('#icon').attr('src', './icons/04d.png');
-          }
-          if (forecast.weather[1].icon === '04n') {
-            $('#icon').attr('src', './icons/04n.png');
-          }
-          if (forecast.weather[1].icon === '09d') {
-            $('#icon').attr('src', './icons/09d.png');
-          }
-          if (forecast.weather[1].icon === '09n') {
-            $('#icon').attr('src', './icons/09n.png');
-          }
-          if (forecast.weather[1].icon === '10d') {
-            $('#icon').attr('src', './icons/10d.png');
-          }
-          if (forecast.weather[1].icon === '10n') {
-            $('#icon').attr('src', './icons/10n.png');
-          }
-          if (forecast.weather[1].icon === '11d') {
-            $('#icon').attr('src', './icons/11d.png');
-          }
-          if (forecast.weather[1].icon === '11n') {
-            $('#icon').attr('src', './icons/11n.png');
-          }
-          if (forecast.weather[1].icon === '13d') {
-            $('#icon').attr('src', './icons/13d.png');
-          }
-          if (forecast.weather[1].icon === '13n') {
-            $('#icon').attr('src', './icons/13n.png');
-          }
-          if (forecast.weather[1].icon === '50d') {
-            $('#icon').attr('src', './icons/50d.png');
-          }
-          if (forecast.weather[1].icon === '50n') {
-            $('#icon').attr('src', './icons/50n.png');
-          }
-          else if ($('#icon').attr('src') === ''){
-            $('#icon').css('display', 'none');
-          }
+        else if (forecast.weather[1].icon === '01d') {
+          $('#icon').attr('src', './icons/01d.png');
         }
-      // }
+        else if (forecast.weather[1].icon === '01n') {
+          $('#icon').attr('src', './icons/01n.png');
+        }
+        else if (forecast.weather[1].icon === '02d') {
+          $('#icon').attr('src', './icons/02d.png');
+        }
+        else if (forecast.weather[1].icon === '02n') {
+          $('#icon').attr('src', './icons/02n.png');
+        }
+        else if (forecast.weather[1].icon === '03d') {
+          $('#icon').attr('src', './icons/03d.png');
+        }
+        else if (forecast.weather[1].icon === '03n') {
+          $('#icon').attr('src', './icons/03n.png');
+        }
+        else if (forecast.weather[1].icon === '04d') {
+          $('#icon').attr('src', './icons/04d.png');
+        }
+        else if (forecast.weather[1].icon === '04n') {
+          $('#icon').attr('src', './icons/04n.png');
+        }
+        else if (forecast.weather[1].icon === '09d') {
+          $('#icon').attr('src', './icons/09d.png');
+        }
+        else if (forecast.weather[1].icon === '09n') {
+          $('#icon').attr('src', './icons/09n.png');
+        }
+        else if (forecast.weather[1].icon === '10d') {
+          $('#icon').attr('src', './icons/10d.png');
+        }
+        else if (forecast.weather[1].icon === '10n') {
+          $('#icon').attr('src', './icons/10n.png');
+        }
+        else if (forecast.weather[1].icon === '11d') {
+          $('#icon').attr('src', './icons/11d.png');
+        }
+        else if (forecast.weather[1].icon === '11n') {
+          $('#icon').attr('src', './icons/11n.png');
+        }
+        else if (forecast.weather[1].icon === '13d') {
+          $('#icon').attr('src', './icons/13d.png');
+        }
+        else if (forecast.weather[1].icon === '13n') {
+          $('#icon').attr('src', './icons/13n.png');
+        }
+        else if (forecast.weather[1].icon === '50d') {
+          $('#icon').attr('src', './icons/50d.png');
+        }
+        else if (forecast.weather[1].icon === '50n') {
+          $('#icon').attr('src', './icons/50n.png');
+        }
+        else if ($('#icon').attr('src') === '') {
+          $('#icon').css('display', 'none');
+        }
+      }
     });
   }
-
-
   getLocation();
-
 });
