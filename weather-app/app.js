f4f9f94bb6ee79229b18e79b975d301e1e1f0a9f
Tim Wheeler <tim@timwheeler.com>
2018-05-22 22:52:20 +0000

    Weather App Version 1.2.0

--- weather-app/app.js
+++ weather-app/app.js
@@ -67,69 +67,69 @@ document.addEventListener('DOMContentLoaded', function() {
           $('#icon').attr('src', forecast.weather[0].icon);
         }
 
-        else if (typeof forecast.weather[1] === 'undefined') {
+        else if (typeof forecast.weather[1] === 'undefined'){
           $('#icon').attr('src', './icons/00.png');
         }
 
         // else if (forecast.weather[1].hasOwnProperty('icon')) {
-        if (forecast.weather[1].icon === '01d') {
-          $('#icon').attr('src', './icons/01d.png');
+          if (forecast.weather[1].icon === '01d') {
+            $('#icon').attr('src', './icons/01d.png');
+          }
+          if (forecast.weather[1].icon === '01n') {
+            $('#icon').attr('src', './icons/01n.png');
+          }
+          if (forecast.weather[1].icon === '02d') {
+            $('#icon').attr('src', './icons/02d.png');
+          }
+          if (forecast.weather[1].icon === '02n') {
+            $('#icon').attr('src', './icons/02n.png');
+          }
+          if (forecast.weather[1].icon === '03d') {
+            $('#icon').attr('src', './icons/03d.png');
+          }
+          if (forecast.weather[1].icon === '03n') {
+            $('#icon').attr('src', './icons/03n.png');
+          }
+          if (forecast.weather[1].icon === '04d') {
+            $('#icon').attr('src', './icons/04d.png');
+          }
+          if (forecast.weather[1].icon === '04n') {
+            $('#icon').attr('src', './icons/04n.png');
+          }
+          if (forecast.weather[1].icon === '09d') {
+            $('#icon').attr('src', './icons/09d.png');
+          }
+          if (forecast.weather[1].icon === '09n') {
+            $('#icon').attr('src', './icons/09n.png');
+          }
+          if (forecast.weather[1].icon === '10d') {
+            $('#icon').attr('src', './icons/10d.png');
+          }
+          if (forecast.weather[1].icon === '10n') {
+            $('#icon').attr('src', './icons/10n.png');
+          }
+          if (forecast.weather[1].icon === '11d') {
+            $('#icon').attr('src', './icons/11d.png');
+          }
+          if (forecast.weather[1].icon === '11n') {
+            $('#icon').attr('src', './icons/11n.png');
+          }
+          if (forecast.weather[1].icon === '13d') {
+            $('#icon').attr('src', './icons/13d.png');
+          }
+          if (forecast.weather[1].icon === '13n') {
+            $('#icon').attr('src', './icons/13n.png');
+          }
+          if (forecast.weather[1].icon === '50d') {
+            $('#icon').attr('src', './icons/50d.png');
+          }
+          if (forecast.weather[1].icon === '50n') {
+            $('#icon').attr('src', './icons/50n.png');
+          }
+          else if ($('#icon').attr('src') === ''){
+            $('#icon').css('display', 'none');
+          }
         }
-        if (forecast.weather[1].icon === '01n') {
-          $('#icon').attr('src', './icons/01n.png');
-        }
-        if (forecast.weather[1].icon === '02d') {
-          $('#icon').attr('src', './icons/02d.png');
-        }
-        if (forecast.weather[1].icon === '02n') {
-          $('#icon').attr('src', './icons/02n.png');
-        }
-        if (forecast.weather[1].icon === '03d') {
-          $('#icon').attr('src', './icons/03d.png');
-        }
-        if (forecast.weather[1].icon === '03n') {
-          $('#icon').attr('src', './icons/03n.png');
-        }
-        if (forecast.weather[1].icon === '04d') {
-          $('#icon').attr('src', './icons/04d.png');
-        }
-        if (forecast.weather[1].icon === '04n') {
-          $('#icon').attr('src', './icons/04n.png');
-        }
-        if (forecast.weather[1].icon === '09d') {
-          $('#icon').attr('src', './icons/09d.png');
-        }
-        if (forecast.weather[1].icon === '09n') {
-          $('#icon').attr('src', './icons/09n.png');
-        }
-        if (forecast.weather[1].icon === '10d') {
-          $('#icon').attr('src', './icons/10d.png');
-        }
-        if (forecast.weather[1].icon === '10n') {
-          $('#icon').attr('src', './icons/10n.png');
-        }
-        if (forecast.weather[1].icon === '11d') {
-          $('#icon').attr('src', './icons/11d.png');
-        }
-        if (forecast.weather[1].icon === '11n') {
-          $('#icon').attr('src', './icons/11n.png');
-        }
-        if (forecast.weather[1].icon === '13d') {
-          $('#icon').attr('src', './icons/13d.png');
-        }
-        if (forecast.weather[1].icon === '13n') {
-          $('#icon').attr('src', './icons/13n.png');
-        }
-        if (forecast.weather[1].icon === '50d') {
-          $('#icon').attr('src', './icons/50d.png');
-        }
-        if (forecast.weather[1].icon === '50n') {
-          $('#icon').attr('src', './icons/50n.png');
-        }
-        else if ($('#icon').attr('src') === '') {
-          $('#icon').css('display', 'none');
-        }
-      }
       // }
     });
   }
